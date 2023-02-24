52b396dcc168a0ef2aa3fd4780b1ee9d25d09be4
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-02-24 15:37:12 +0100

    function complete now add the color

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,7 +1,18 @@
+// Get the const for the main container
 const containerDom = document.querySelector('.container');
 
-for (let i = 1; i <= 0; i++) {
-    const boxDom = '<div class="numberBox">${i}</div>';
-    containerDom.innerHTML +=  boxDom;
+// Apply the loop with let(i) from 1 to 100
+for (let i = 1; i <= 100; i++) {
+  let boxDom = `<div class="boxDom">${i}</div>`;
 
+  // color different box if are multipliers of 3, 5, or both
+  if (i % 3 === 0 && i % 5 === 0) {
+    boxDom = `<div class="boxDom fizzbuzz">BuzzFizz</div>`;
+  } else if (i % 3 === 0) {
+    boxDom = `<div class="boxDom fizz">Buzz</div>`;
+  } else if (i % 5 === 0) {
+    boxDom = `<div class="boxDom buzz">Fizz</div>`;
+  }
+
+  containerDom.innerHTML += boxDom;
 }
